c773589ea204cbacb1c1bf54bb2cb822aa9f8d7c
seif hassan <seifhassan6996@gmail.com>
2025-08-18 12:42:02 +0300

    change in model alert and testing the new smoke alert w 4krn

--- src/server/models/alerts.js
+++ src/server/models/alerts.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const alertSchema = new mongoose.Schema({
   datacenterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Datacenter', required: true },
-  type: { type: String, enum: ['temperature', 'humidity', 'power', 'security'], required: true },
+  type: { type: String, enum: ['temperature', 'humidity', 'power', 'security','smoke'], required: true },
   severity: { type: String, enum: ['warning', 'critical'], required: true },
   message: { type: String, required: true },
   status: { type: String, enum: ['new', 'acknowledged', 'resolved'], default: 'new' },
