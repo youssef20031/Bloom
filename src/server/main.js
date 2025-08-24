a169c3e20c13b0266b2dfab9810ad4d0a0833feb
Youssef Maged <youssef.maged.6000@gmail.com>
2025-08-24 17:04:07 +0300

    UI

--- src/server/main.js
+++ src/server/main.js
@@ -50,7 +50,6 @@ mongoose.connect(MONGO_URI, {
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   heartbeatFrequencyMS: 10000, // Increase heartbeat frequency to 10 seconds
 })
   .then(() => {
