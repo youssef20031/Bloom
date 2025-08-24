1c86f9e03edf95ec3e805f3dd516d9c0230fdf2c
Youssef Maged <youssef.maged.6000@gmail.com>
2025-08-24 12:11:26 +0300

    UI

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {
+        allowedHosts: ['bloom-86z2.onrender.com'],
         proxy: {
             // This tells Vite to forward any request that starts with /api
             // to your backend server running on http://localhost:3000
